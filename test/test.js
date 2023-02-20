7a37e4b56312863199153460ab1eb942225240db
Edwin <ed107107@gmail.com>
2023-02-20 17:42:16 +0300

    test using mocha & chai first commit

--- test/test.js
+++ test/test.js
@@ -6,19 +6,22 @@ const multiply = require('../multiply')
 // eslint-disable-next-line no-undef
 describe('****** Multiply function Test ******', () => {
   // eslint-disable-next-line no-undef
-  it('1*1 should equal to 1', () => {
+  it('1 * 1 should equal to 1', () => {
     assert.equal(multiply(1, 1), 1)
   })
   // eslint-disable-next-line no-undef
-  it('2*2 should equal to 4', () => {
+  it('2 * 2 should equal to 4', () => {
     assert.equal(multiply(2, 2), 4)
   })
   // eslint-disable-next-line no-undef
-  it('3*3 should equal to 9', () => {
+  it('3 * 3 should equal to 9', () => {
     assert.equal(multiply(3, 3), 9)
   })
   // eslint-disable-next-line no-undef
-  it('4*4 should equal to 16', () => {
+  it('4 * 4 should equal to 16', () => {
     assert.equal(multiply(4, 4), 16)
+  })
+  it('23 * 45 should equal to 23 * 45', () => {
+    assert.equal(multiply(23, 45), 23 * 45)
   })
 })
